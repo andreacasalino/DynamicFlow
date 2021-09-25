6cc496bb430014d145b93de98b1af0a5995eaa40
Andrea <andrecasa91@gmail.com>
2021-09-25 22:54:28 +0100

    auto unamed counter mechanism

--- src/src/flow/FlowEntity.cpp
+++ src/src/flow/FlowEntity.cpp
@@ -6,9 +6,19 @@
  **/
 
 #include <flow/FlowEntity.h>
+#include <sstream>
 
 namespace flw {
+    static long long unamed_counter = 0;
+
     FlowEntity::FlowEntity(const std::string& nameRaw) {
+        if (nameRaw.empty()) {
+            std::stringstream stream;
+            stream << "Unamed-" << unamed_counter;
+            ++unamed_counter;
+            name = std::make_shared<const std::string>(stream.str());
+            return;
+        }
         name = std::make_shared<const std::string>(nameRaw);
     };
 }
